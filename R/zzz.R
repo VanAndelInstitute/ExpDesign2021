3ee2ca5eeab1c6415d7f1da239090c13b73cae10
Tim Triche, Jr <tim.triche@gmail.com>
2021-09-27 10:32:55 -0400

    more helpful warning messages

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,9 @@
   googlesheets4::gs4_deauth() 
   message("googlesheets4::gs4_deauth() called to reduce annoyances.")
 
+  googlesheets4::gs4_user()
+  message("help(googlesheets4::gs4_auth) # if you want to log back in")
+
   suppressWarnings(reqInstall("tidytext")) # test a recent dependency 
   message("ExpDesign2021 package and dependencies loaded.")
 
