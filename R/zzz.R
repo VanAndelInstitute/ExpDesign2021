fc58b9f495e80bff29738e0a84ca4991453a3be9
Tim Triche, Jr <tim.triche@gmail.com>
2021-10-03 22:31:24 -0400

    trigger vignette builds and bump version

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,8 @@
 .onAttach <- function (lib, pkgname="ExpDesign2021") {
 
+  #suppressPackageStartupMessages()
   googlesheets4::gs4_deauth() 
-  message("googlesheets4::gs4_deauth() called to reduce annoyances.")
-
   googlesheets4::gs4_user()
-  message("help(googlesheets4::gs4_auth) # if you want to log back in")
-
   suppressWarnings(reqInstall("tidytext")) # test a recent dependency 
-  message("ExpDesign2021 package and dependencies loaded.")
 
 }
