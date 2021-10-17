e60e2490c8c840f32829f908749906ae03573938
Tim Triche, Jr <tim.triche@gmail.com>
2021-10-17 15:41:39 -0400

    updates

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,24 @@
+#' @rdname ExpDesign2021 
+#' 
+#' A package for the 2021 edition of Experimental Design and Biostatistics.
+#'
+#' @examples 
+#' 
+#' # phelp == 'package help'
+#' phelp(ExpDesign2021) 
+#' 
+#' # automate fetching assignment data 
+#' fetchAssignments()
+#'
+#' # list object sizes (lsos) and pipe the output into head (default: 6 lines)
+#' lsos() %>% head 
+#' 
 .onAttach <- function (lib, pkgname="ExpDesign2021") {
 
-  #suppressPackageStartupMessages()
+  tidymodels_prefer(quiet=FALSE)
+  conflicted::conflict_prefer("filter", "dplyr")
+  conflicted::conflict_prefer("mutate", "dplyr")
   googlesheets4::gs4_deauth() 
   googlesheets4::gs4_user()
-  suppressWarnings(reqInstall("tidytext")) # test a recent dependency 
 
 }
